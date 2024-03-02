46dd70c2962867f1e9f76f767420864bdab42564
CodeChainWizard <shubhamdanecha789@gmail.com>
2024-03-02 20:14:17 +0530

    Create Card Componet and show card details

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -72,60 +72,30 @@ class HomePage extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 children: <Widget>[
                   Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                        color: Colors.black,
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      width: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    child: const Text("All", textAlign: TextAlign.center, style: TextStyle(height: 2),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    width: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
-                    child: const Text("Nearby", textAlign: TextAlign.center, style: TextStyle(height: 2),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    width: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
-                    child: const Text("Pizza", textAlign: TextAlign.center, style: TextStyle(height: 2),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    width: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
-                    child: const Text("Pissta", textAlign: TextAlign.center, style: TextStyle(height: 2),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    width: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                        color: Colors.black,
-                      ),
-                    ),
-                    child: const Text("Burger", textAlign: TextAlign.center, style: TextStyle(height: 2),),
-                  ),
+                      child: Container(
+                        margin: EdgeInsets.only(left: 7.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.menu_sharp),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            const Text(
+                              "All",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(height: 2),
+                            ),
+                          ],
+                        ),
+                      )),
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 10.0),
                     width: 100,
@@ -135,9 +105,240 @@ class HomePage extends StatelessWidget {
                         color: Colors.black,
                       ),
                     ),
-                    child: const Text("Ice-cream", textAlign: TextAlign.center, style: TextStyle(height: 2),),
+                    child: Container(
+                      margin: EdgeInsets.only(left: 7.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.location_on),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "Nearby",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
+                    width: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.only(left: 7.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.local_pizza_outlined),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "Pizza",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
+                    width: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.only(left: 7.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.emoji_food_beverage_outlined),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "Pasta",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
+                    width: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.only(left: 7.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.fastfood),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "Burger",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
+                    width: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.only(left: 7.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.icecream_sharp),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "Ice-cream",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 2),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.only(top: 20.0),
+              elevation: 4,
+              child: SizedBox(
+                width: 150,
+                height: 190,
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                          width: 150,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              "assets/images/food1.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                          )),
+                      Container(
+                        margin: EdgeInsets.only(top: 5.0),
+                        child: const Text("Food 1 Name"),
+                      ),
+                      const Text(
+                        "\$10.0",
+                        style: TextStyle(color: Colors.black45),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.star),
+                          Text("4.5"),
+                          Spacer(),
+                          Container(
+                            margin: EdgeInsets.only(left: 20.0),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Icon(
+                              Icons.favorite_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.only(top: 20.0),
+              elevation: 4,
+              child: Container(
+                width: 150,
+                height: 190,
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                          width: 150,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              "assets/images/food2.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                          )),
+                      Container(
+                        margin: EdgeInsets.only(top: 5.0),
+                        child: Text("Food 2 Name"),
+                      ),
+                      Text(
+                        "\$10.0",
+                        style: TextStyle(color: Colors.black45),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.star),
+                          Text("4.5"),
+                          Spacer(),
+                          Container(
+                            margin: EdgeInsets.only(left: 20.0),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Icon(
+                              Icons.favorite_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
